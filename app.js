3d52115c4b7156b23ab5793d80d68fe30125299f
özcan çetin <cetin445@gmail.com>
2022-06-17 12:02:47 +0300

    variables was defined

---
+++ app.js
@@ -0,0 +1,5 @@
+//* VARIABLES //
+const form = document.querySelector("section.first form");
+const input = document.querrySelector(".first input");
+const msg = document.querySelector("span.msg");
+const list = document.querrySelector(".cities");
