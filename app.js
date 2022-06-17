0ba8d6cfa8a3014382d410271e422c1267f6ecd4
özcan çetin <cetin445@gmail.com>
2022-06-17 13:01:15 +0300

    The api key is encrypted and sent to the local storage section.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //* VARIABLES //
 const form = document.querySelector("section.first form");
-const input = document.querrySelector(".first input");
+const input = document.querySelector(".first input");
 const msg = document.querySelector("span.msg");
-const list = document.querrySelector(".cities");
+const list = document.querySelector(".cities");
+
+//! burada api key i şifreli olarak local storage gönderdim. Sonra da buradan kaldırdım. Api key kullanmam gerektiğinde şifreyi çözerek kullanacağım.
+
