26351f5a55732b9da54d6d081d7a8a4ed8613580
özcan çetin <cetin445@gmail.com>
2022-06-17 13:07:37 +0300

    submit function was prevent default in form section

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const list = document.querySelector(".cities");
 
 //! burada api key i şifreli olarak local storage gönderdim. Sonra da buradan kaldırdım. Api key kullanmam gerektiğinde şifreyi çözerek kullanacağım.
 
+
+//forma her submit yaptığımda sayfa yenilenmesin diye yani submit fonksiyonu benim istediğim şekilde davransın diye burada prevent default yaptım.
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    getWeatherDataFromApi();
+})
